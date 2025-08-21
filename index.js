70bd555dbf88c51435749d6662b5951fb4b867ed
markimagemTv <markimagem2014@gmail.com>
2025-08-21 13:25:26 -0300

    Adiciona a estrutura inicial do bot Telegram + WhatsApp (Baileys) - index.js

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-// index.js
-const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
-const TelegramBot = require('node-telegram-bot-api');
+// Código inicial do bot Telegram + WhatsApp (Baileys)
 
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || 'SEU_TOKEN_DO_TELEGRAM';
+const { WAConnection } = require('@adiwajshing/baileys');
 
-const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+const startBot = async () => {
+    const conn = new WAConnection();
+    conn.on('qr', (qr) => {
+        console.log('QR Code recebido:', qr);
+    });
 
-async function iniciarWhatsApp() {
-  const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
-  const sock = makeWASocket({ auth: state });
+    await conn.connect();
+    console.log('Bot conectado como:', conn.user.jid);
+};
 
-  sock.ev.on('creds.update', saveCreds);
-
-  return sock;
-}
-
-let whatsappSock;
-
-iniciarWhatsApp().then(sock => {
-  whatsappSock = sock;
-});
-
-bot.onText(/\/agendar (.+)/, async (msg, match) => {
-  const chatId = msg.chat.id;
-  const [numero, horario, ...mensagemParts] = match[1].split(' ');
-  const mensagem = mensagemParts.join(' ');
-
-  bot.sendMessage(chatId, `Mensagem agendada para ${numero} às ${horario}: \"${mensagem}\"`);
-
-  setTimeout(async () => {
-    if (whatsappSock) {
-      await whatsappSock.sendMessage(numero + '@s.whatsapp.net', { text: mensagem });
-      bot.sendMessage(chatId, `Mensagem enviada para ${numero} via WhatsApp!`);
-    } else {
-      bot.sendMessage(chatId, `WhatsApp não está conectado!`);
-    }
-  }, calcularTimeout(horario));
-});
-
-function calcularTimeout(horario) {
-  return 10000; // ajuste para o seu formato
-}
+startBot();
